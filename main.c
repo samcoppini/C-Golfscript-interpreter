96ad76f01fdedf86909b5e3ae4f1b87b6e64140e
Sam Coppini <soc27@humboldt.edu>
2017-09-05 08:21:05 -0700

    Add ability to run scripts on the command line

--- main.c
+++ main.c
@@ -1,19 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "golf.h"
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
+  const char *filename = NULL;
+  const char *command_text = NULL;
+
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] != '-') {
+      if (filename != NULL) {
+        error("The interpreter can only handle one file at once!");
+      }
+      filename = argv[i];
+    }
+    else if (strcmp(argv[i], "-r") || strcmp(argv[i], "--run")) {
+      if (command_text != NULL) {
+        error("Can only run one command-line argument script at a time!");
+      }
+      else if (++i == argc) {
+        error("No command-line script given to run!");
+      }
+      command_text = argv[i];
+    }
+    else {
+      error("Invalid command-line argument '%s'!", argv[i]);
+    }
+  }
+
+  if (filename == NULL && command_text == NULL) {
     error("No golfscript file provided!");
   }
-
-  FILE *file = fopen(argv[1], "r");
-  if (file == NULL) {
-    error("Unable to open '%s'!", argv[1]);
+  else if (filename != NULL && command_text != NULL) {
+    error("Can only run either a command-line script or file, not both!");
   }
 
-  String code = read_file_to_string(file);
-  fclose(file);
+  String code;
+
+  if (filename) {
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+      error("Unable to open '%s'!", argv[1]);
+    }
+    code = read_file_to_string(file);
+    fclose(file);
+  }
+  else if (command_text) {
+    code = create_string(command_text);
+  }
+
   init_interpreter();
   execute_string(&code);
   end_interpreter();
